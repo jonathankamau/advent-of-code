6d4021ddc030230d1e6e1d71321e0ac85ad9ceb1
jonathankamau <kamaujonathan@gmail.com>
2020-12-07 21:39:27 +0300

    solution for day 1

--- 2020/Day1/solution.py
+++ 2020/Day1/solution.py
@@ -4,13 +4,10 @@ class Sum:
     def readFile(self):
 
         nums = []
-
         file = open('input.txt', 'r')
 
         for line in file:
-
             nums.append(int(line))
-
         return nums
 
     def getTwoSum(self, year):
@@ -18,11 +15,8 @@ class Sum:
         nums = self.readFile()
 
         for num in nums:
-
             second_num = year - num
-
             if second_num in nums:
-
                 result = second_num * num
                 break
             else:
@@ -38,18 +32,12 @@ class Sum:
         second_pair = 0
 
         for num in nums:
-
             second_pair = year - num
-
             second_result = self.getTwoSum(second_pair)
-
             if second_result > 0:
-
                 result = num * second_result
                 break
-            
             else:
-
                 result = 0
 
         return result
